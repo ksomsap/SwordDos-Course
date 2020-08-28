67ed546bc87abc6e1afa914428e01d358b1bbe87
ksomsap <keittisakcpm@gmail.com>
2020-08-28 19:46:45 +0700

    First Test

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 2f;
+    Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new Vector2(0, 0);
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
